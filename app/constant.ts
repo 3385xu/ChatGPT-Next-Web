28068b2de70839812b304f1057c7bac55c198847
3385xu <42068209+3385xu@users.noreply.github.com>
2024-11-22 23:59:03 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -317,9 +317,17 @@ const openaiModels = [
 ];
 
 const googleModels = [
-  "gemini-1.0-pro",
+  "gemini-1.0-pro-latest",
   "gemini-1.5-pro-latest",
+  "gemini-1.5-pro-001",
+  "gemini-1.5-pro-002",
+  "gemini-1.5-pro-exp-0801",
+  "gemini-1.5-pro-exp-0827",
   "gemini-1.5-flash-latest",
+  "gemini-1.5-flash-001",
+  "gemini-1.5-flash-002",
+  "gemini-1.5-flash-exp-0827",
+  "gemini-1.5-flash-8b-exp-0924",
   "gemini-exp-1114",
   "gemini-exp-1121",
   "learnlm-1.5-pro-experimental",
